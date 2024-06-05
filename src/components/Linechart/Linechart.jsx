4deac40486fb5f94541085d7fdb4c5df55acce98
tech-in-tech <anubhavsharma06dec@gmail.com>
2024-06-05 18:13:23 +0530

    add coin info table in coin page and make it responsive project completed

---
+++ src/components/Linechart/Linechart.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+import Chart from 'react-google-charts'
+
+const Linechart = ({historicalData}) => {
+  const [data,setData] = useState([["Date",'Prices']])
+  useEffect(()=>{
+    let dataCopy = [["data","Prices"]];
+    if(historicalData.prices){
+      historicalData.prices.map((item)=>{
+        dataCopy.push([`${new Date(item[0]).toLocaleDateString().slice(0,-5)}`,item[1]])
+      })
+      setData(dataCopy);
+    }
+  },[historicalData])
+  return (
+    <Chart
+      chartType='LineChart'
+      data={data}
+      height="100%"
+      legendToggle
+    />
+  )
+}
+
+export default Linechart
